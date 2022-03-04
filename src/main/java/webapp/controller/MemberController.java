5d363cd50956009011df253d345ea7d12d999e8d
yongbeomj <alex.ybjang@gmail.com>
2022-03-04 18:02:28 +0900

    로그인 완료

--- src/main/java/webapp/controller/MemberController.java
+++ src/main/java/webapp/controller/MemberController.java
@@ -2,6 +2,7 @@ package webapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +10,7 @@ import webapp.domain.dto.MemberDto;
 import webapp.service.MemberService;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.net.http.HttpRequest;
 
 @Controller
@@ -22,29 +24,44 @@ public class MemberController {
 
     // 회원가입
     @GetMapping("/member/signup")
-    public String Signup(){
+    public String signup() {
         return "member/signup";
     }
 
-    @PostMapping("/member/signupcontroller")
-    public String signupcontroller(MemberDto memberDto){
+    @PostMapping("/member/signupController")
+    public String signupController(MemberDto memberDto) {
         // 1. 작성 데이터 가져온다. - form name과 dto가 동일하면 자동 주입
         // 2. 서비스에 dto 넘기기
-        memberService.Signup(memberDto);
+        memberService.signup(memberDto);
+        System.out.println("회원가입 성공");
         return "redirect:/";
 
     }
 
     // 로그인
     @GetMapping("/member/login")
-    public String Login(){
+    public String Login() {
         return "member/login";
+    }
+
+    @PostMapping("/member/loginController")
+    public String loginController(MemberDto memberDto) {
+        // 1. 서비스에 dto넘기고 결과값 dto에 저장
+        MemberDto loginDto = memberService.login(memberDto);
+        // 2. 받아온 정보를 세션에 저장
+        if (loginDto != null) {
+            HttpSession session = request.getSession(); // 세션 가져오기
+            session.setAttribute("loginDto", loginDto); // 세션 설정
+            System.out.println("로그인 성공");
+            return "redirect:/";
+        }
+        return null;
     }
 
 
     // 마이페이지
     @GetMapping("/member/memberinfo")
-    public String Memberinfo(){
+    public String Memberinfo() {
         return "member/memberinfo";
     }
 
--- src/main/java/webapp/service/MemberService.java
+++ src/main/java/webapp/service/MemberService.java
@@ -3,7 +3,10 @@ package webapp.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import webapp.domain.dto.MemberDto;
+import webapp.domain.entity.MemberEntity;
 import webapp.domain.entity.MemberRepository;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -12,10 +15,31 @@ public class MemberService {
     MemberRepository memberRepository;
 
     // 회원가입
-    public boolean Signup(MemberDto memberDto){
+    public boolean signup(MemberDto memberDto) {
         // 컨트롤러에서 넘겨받은 데이터를 db에 저장
         memberRepository.save(memberDto.toentity());
-
         return true;
+    }
+
+    // 로그인
+    public MemberDto login(MemberDto memberDto) {
+        // db data 전체 조회
+        List<MemberEntity> memberEntityList = memberRepository.findAll();
+
+//        System.out.println("id : " + memberDto.getMid());
+//        System.out.println("pw : " + memberDto.getMpw());
+
+        // 컨트롤러에서 넘겨받은 데이터를 db와 비교
+        for (MemberEntity memberEntity : memberEntityList) {
+            // 엔티티의 id와 pw가 입력받은(dto) id와 pw가 동일하면
+            if (memberEntity.getMid().equals(memberDto.getMid()) && memberEntity.getMpw().equals(memberDto.getMpw())) {
+                // dto에 빌드
+                return MemberDto.builder()
+                        .mid(memberEntity.getMid())
+                        .mpw(memberEntity.getMpw())
+                        .build();
+            }
+        }
+        return null;
     }
 }
