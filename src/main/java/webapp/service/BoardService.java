a980a038d2c2da387ac6d5c9ee9fa49f39029e13
yongbeomj <alex.ybjang@gmail.com>
2022-03-22 18:27:30 +0900

    무한 대댓글 list 진행 중

--- src/main/java/webapp/service/BoardService.java
+++ src/main/java/webapp/service/BoardService.java
@@ -161,6 +161,7 @@ public class BoardService {
     public boolean rereplywrite(int rno, String rcontents) {
         // 해당 댓글 가져오기
         Optional<ReplyEntity> replyEntities = replyRepository.findById(rno);
+//        System.out.println("rno : " + rno);
         // 게시물 가져오기
         int bno = replyEntities.get().getBoardEntity().getBno();
         Optional<BoardEntity> boardEntity = boardRepository.findById(bno);
@@ -175,18 +176,25 @@ public class BoardService {
         // 깊이는 해당 댓글의 +1
         // 순서는 +1 깊이에서 같은 부모의 순서 최대값 +1
         // 중간에 댓글이 등록될 경우 뒷 순서 댓글은 하나씩 밀려야 함
-        List<ReplyEntity> reply = replyRepository.findAll();
+
+        // 부모는 그대로
         int rparent = replyEntities.get().getRparent();
+        // 뎁스, 순서는 각각 +1
         int rdepth = replyEntities.get().getRdepth() + 1;
         int temporder = replyEntities.get().getRorder() + 1;
+        System.out.println("rparent : " + rparent);
+        System.out.println("rdepth : " + rdepth);
+        System.out.println("temporder : " + temporder);
+
+        // 만약 부모가 같고 신규댓글 순서 이상인 댓글들은 모두 순서를 +1
+        List<ReplyEntity> reply = replyRepository.findAll();
+
         int maxrorder;
         try {
-            int rorder = reply.get(0).getRparent();
+            int rorder = reply.get(0).getRorder();
             for (int i = 0; i < reply.size(); i++) {
-                if (reply.get(i).getRparent() == rparent) {
-                    if (rorder < reply.get(i).getRorder()) {
-                        rorder = reply.get(i).getRorder();
-                    }
+                if (reply.get(i).getRparent() == rparent && reply.get(i).getRorder() == temporder) {
+                    reply.get(i).setRorder(reply.get(i).getRorder() + 1);
                 }
             }
             maxrorder = rorder + 1;
@@ -209,7 +217,21 @@ public class BoardService {
 
         return false;
     }
+    // boardupdate
+    @Transactional
+    public boolean boardupdate(BoardDto boardDto) {
+        try {
+            Optional<BoardEntity> entityOptional = boardRepository.findById(boardDto.getBno());
+            entityOptional.get().setBtitle(boardDto.getBtitle());
+            entityOptional.get().setBcontents(boardDto.getBcontents());
+            return true;
+        } catch (Exception e) {
+            System.out.println("에러");
+            System.out.println(e);
+            return false;
+        }
 
+    }
 
 //    // 모든 댓글 출력 - 수정 중
 //    public List<ReplyEntity> getreplylist(int bno) {
