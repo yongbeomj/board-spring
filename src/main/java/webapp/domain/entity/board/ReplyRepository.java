4e8781bcc03de190d9cc9529ce00f3a3dc606f50
JYB <alex.ybjang@gmail.com>
2022-03-25 16:13:35 +0900

    무한 대댓글 오름차순 정렬 완료

--- src/main/java/webapp/domain/entity/board/ReplyRepository.java
+++ src/main/java/webapp/domain/entity/board/ReplyRepository.java
@@ -15,10 +15,6 @@ public interface ReplyRepository extends JpaRepository<ReplyEntity, Integer> {
     @Query(nativeQuery = true, value = "SELECT * FROM reply where bno = :bno and rparent = :rparent and rorder = :rorder")
     List<ReplyEntity> nextreply(@Param("bno") int bno, @Param("rparent") int rparent, @Param("rorder") int rorder);
 
-//    // 다음 뎁스 댓글 여부 확인
-//    @Query(nativeQuery = true, value = "SELECT * FROM reply where bno = :bno and rparent = :rparent and rdepth = :rdepth")
-//    List<ReplyEntity> findreply(@Param("bno") int bno, @Param("rparent") int rparent, @Param("rdepth") int rdepth);
-
     // 같은 부모 댓글 가져오기
     @Query(nativeQuery = true, value = "SELECT * FROM reply where bno = :bno and rparent = :rparent")
     List<ReplyEntity> findreply(@Param("bno") int bno, @Param("rparent") int rparent);
